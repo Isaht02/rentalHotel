d1df6798d6eeb46b5922ab3e9839234df0a000e3
ThaiXutions <36483534+ThaiXutions@users.noreply.github.com>
2022-12-13 10:49:30 +0700

    update table room, roomtypes sql

--- rentalhotel.sql
+++ rentalhotel.sql
@@ -1,225 +1,125 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 12, 2022 at 03:49 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.4
+/*
+ Navicat Premium Data Transfer
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+ Source Server         : sampviet
+ Source Server Type    : MySQL
+ Source Server Version : 100425
+ Source Host           : localhost:3306
+ Source Schema         : rentalhotel
 
+ Target Server Type    : MySQL
+ Target Server Version : 100425
+ File Encoding         : 65001
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+ Date: 13/12/2022 10:48:39
+*/
 
---
--- Database: `rentalhotel`
---
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Table structure for accounts
+-- ----------------------------
+DROP TABLE IF EXISTS `accounts`;
+CREATE TABLE `accounts`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `firstname` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `lastname` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `phone` int(12) NULL DEFAULT NULL,
+  `gender` int(11) NULL DEFAULT NULL,
+  `type` int(11) NULL DEFAULT 0,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
---
--- Table structure for table `accounts`
---
+-- ----------------------------
+-- Records of accounts
+-- ----------------------------
+INSERT INTO `accounts` VALUES (1, 'lanvo34', '$2b$10$qs5sZqiweccHMt/6LcVCse9xc5ZvXVyCpuUvOp19z10xR/BJsqdri', 'Lan', 'Vo', '24/7 A', 764984824, 0, 0);
+INSERT INTO `accounts` VALUES (2, 'v.iaht', '$2b$10$fZpoAeUZtaki13gT8Fxh2e9Y3aAIad1PZW6Lpto6HBK12PsBgD8Qe', 'Thai', 'Vo', '24/7 B', 764984824, 1, 0);
 
-CREATE TABLE `accounts` (
-  `id` int(11) NOT NULL,
-  `username` varchar(64) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `firstname` varchar(128) DEFAULT NULL,
-  `lastname` varchar(128) DEFAULT NULL,
-  `address` varchar(255) DEFAULT NULL,
-  `phone` int(12) DEFAULT NULL,
-  `gender` int(11) DEFAULT NULL,
-  `type` int(11) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+-- ----------------------------
+-- Table structure for payments
+-- ----------------------------
+DROP TABLE IF EXISTS `payments`;
+CREATE TABLE `payments`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `transcation_id` int(12) NULL DEFAULT NULL,
+  `customer_id` int(12) NULL DEFAULT NULL,
+  `total_price` int(12) NULL DEFAULT NULL,
+  `date` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
---
--- Dumping data for table `accounts`
---
+-- ----------------------------
+-- Table structure for reservations
+-- ----------------------------
+DROP TABLE IF EXISTS `reservations`;
+CREATE TABLE `reservations`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) NULL DEFAULT NULL,
+  `room_id` int(11) NULL DEFAULT NULL,
+  `date` datetime(0) NULL DEFAULT NULL,
+  `date_in` date NULL DEFAULT NULL,
+  `date_out` date NULL DEFAULT NULL,
+  `total_payment` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
-INSERT INTO `accounts` (`id`, `username`, `password`, `firstname`, `lastname`, `address`, `phone`, `gender`, `type`) VALUES
-(1, 'lanvo34', '$2b$10$qs5sZqiweccHMt/6LcVCse9xc5ZvXVyCpuUvOp19z10xR/BJsqdri', 'Lan', 'Vo', '24/7 A', 764984824, 0, 0),
-(2, 'v.iaht', '$2b$10$fZpoAeUZtaki13gT8Fxh2e9Y3aAIad1PZW6Lpto6HBK12PsBgD8Qe', 'Thai', 'Vo', '24/7 B', 764984824, 1, 0);
+-- ----------------------------
+-- Table structure for rooms
+-- ----------------------------
+DROP TABLE IF EXISTS `rooms`;
+CREATE TABLE `rooms`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `price` int(12) NULL DEFAULT NULL,
+  `type` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `bedNum` int(12) NULL DEFAULT NULL,
+  `peopleNum` int(12) NULL DEFAULT NULL,
+  `img` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Records of rooms
+-- ----------------------------
+INSERT INTO `rooms` VALUES (1, 'Suit Room', 100, '1', 2, 3, NULL, NULL);
+INSERT INTO `rooms` VALUES (2, 'Standard Room', 200, '2', 2, 4, NULL, NULL);
+INSERT INTO `rooms` VALUES (3, 'Family Room', 300, '3', 2, 5, NULL, NULL);
+INSERT INTO `rooms` VALUES (4, 'Deluxury Room', 100, '1', 2, 3, NULL, NULL);
+INSERT INTO `rooms` VALUES (5, 'Luxyry Room', 370, '3', 2, 5, NULL, NULL);
 
---
--- Table structure for table `payments`
---
+-- ----------------------------
+-- Table structure for roomtypes
+-- ----------------------------
+DROP TABLE IF EXISTS `roomtypes`;
+CREATE TABLE `roomtypes`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
-CREATE TABLE `payments` (
-  `id` int(11) NOT NULL,
-  `transcation_id` int(12) DEFAULT NULL,
-  `customer_id` int(12) DEFAULT NULL,
-  `total_price` int(12) DEFAULT NULL,
-  `date` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- ----------------------------
+-- Records of roomtypes
+-- ----------------------------
+INSERT INTO `roomtypes` VALUES (1, 'small');
+INSERT INTO `roomtypes` VALUES (2, 'medium');
+INSERT INTO `roomtypes` VALUES (3, 'big');
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Table structure for transactions
+-- ----------------------------
+DROP TABLE IF EXISTS `transactions`;
+CREATE TABLE `transactions`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(12) NULL DEFAULT NULL,
+  `reservation_id` int(12) NULL DEFAULT NULL,
+  `payment_id` int(12) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
---
--- Table structure for table `reservations`
---
-
-CREATE TABLE `reservations` (
-  `id` int(11) NOT NULL,
-  `customer_id` int(11) DEFAULT NULL,
-  `room_id` int(11) DEFAULT NULL,
-  `date` datetime DEFAULT NULL,
-  `date_in` date DEFAULT NULL,
-  `date_out` date DEFAULT NULL,
-  `total_payment` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `rooms`
---
-
-CREATE TABLE `rooms` (
-  `id` int(11) NOT NULL,
-  `name` varchar(32) DEFAULT NULL,
-  `price` int(12) DEFAULT NULL,
-  `type` varchar(12) DEFAULT NULL,
-  `bedNum` int(12) DEFAULT NULL,
-  `peopleNum` int(12) DEFAULT NULL,
-  `img` varchar(256) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `rooms`
---
-
-INSERT INTO `rooms` (`id`, `name`, `price`, `type`, `bedNum`, `peopleNum`, `img`) VALUES
-(1, 'room1', 100, 'small', 2, 3, NULL),
-(2, 'room2', 200, 'medium', 2, 4, NULL),
-(3, 'room3', 300, 'big', 2, 5, NULL),
-(4, 'room4', 100, 'small', 2, 3, NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `roomtypes`
---
-
-CREATE TABLE `roomtypes` (
-  `id` int(11) NOT NULL,
-  `type` varchar(32) DEFAULT NULL,
-  `price` int(12) DEFAULT NULL,
-  `bedNum` int(12) DEFAULT NULL,
-  `peopleNum` int(12) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `roomtypes`
---
-
-INSERT INTO `roomtypes` (`id`, `type`, `price`, `bedNum`, `peopleNum`) VALUES
-(1, 'small', 100, 2, 3),
-(2, 'medium', 200, 2, 4),
-(3, 'big', 300, 2, 5);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `transactions`
---
-
-CREATE TABLE `transactions` (
-  `id` int(11) NOT NULL,
-  `customer_id` int(12) DEFAULT NULL,
-  `reservation_id` int(12) DEFAULT NULL,
-  `payment_id` int(12) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `accounts`
---
-ALTER TABLE `accounts`
-  ADD PRIMARY KEY (`id`) USING BTREE;
-
---
--- Indexes for table `payments`
---
-ALTER TABLE `payments`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `reservations`
---
-ALTER TABLE `reservations`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `rooms`
---
-ALTER TABLE `rooms`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `roomtypes`
---
-ALTER TABLE `roomtypes`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `transactions`
---
-ALTER TABLE `transactions`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `accounts`
---
-ALTER TABLE `accounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `payments`
---
-ALTER TABLE `payments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `reservations`
---
-ALTER TABLE `reservations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `rooms`
---
-ALTER TABLE `rooms`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `roomtypes`
---
-ALTER TABLE `roomtypes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `transactions`
---
-ALTER TABLE `transactions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET FOREIGN_KEY_CHECKS = 1;
